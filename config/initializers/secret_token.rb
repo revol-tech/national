b24e7560a1bdd962396010a9b635e1e66aaef4fb
aaganja <kuberaaganja@gmail.com>
2012-04-24 11:17:19 +0545

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NCW::Application.config.secret_token = 'b1198dd22302fdfd68238bc41076bfee025578fc5a57df46df8904c22f00934220a1548e49694e7504fbc1d1543963d44f514036113b7609901faa2c159fb8fb'
