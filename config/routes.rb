d0753a39e4920514a6004ce62e98314f6f7ab563
aaganja <kuberaaganja@gmail.com>
2012-04-30 17:50:35 +0545

    front page almost complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NCW::Application.routes.draw do
+  resources :pages
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   devise_for :users
@@ -54,7 +56,7 @@ NCW::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+    root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
